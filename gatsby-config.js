51d1443520567992bc3d415e484d578f81bd405f
Ben Elliot <belliot@liveperson.com>
2021-08-15 21:39:40 +1000

    Finished Gatsby tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Ben Elliot Blog",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",],
 };
