d37184d19dd658a706be3bd4933e4310893e27fa
Ben Elliot <benjiboi214@gmail.com>
2021-08-20 01:49:30 +1000

    Update site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Systemiphus Blog",
+    title: "Systemiphus",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
